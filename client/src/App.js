885ba6bc4efcd2a81d7c42d27b585be78ef406cf
Gabriel Coronadogit config --global user.name Gabriel <gecoronados@gmail.com>
2021-04-13 11:31:14 -0400

    Commit #1 addint dependencies and part of the backend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./newlogo.svg";
+import Movie from "./components/Movie";
+import styled from "styled-components";
+import Homepage from "./components/Homepage";
+import NavBar from "./components/NavBar/NavBar";
+import GlobalStyle from "./components/GlobalStyle";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <GlobalStyle />
+      <BrowserRouter>
+        <NavBarGridWrapper>
+          <NavBar />
+        </NavBarGridWrapper>
+        <MainPageGridWrapper>
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/movie/:id">
+              <Movie />
+            </Route>
+          </Switch>
+        </MainPageGridWrapper>
+      </BrowserRouter>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-areas:
+    "header header header header header header"
+    "sidebar main main main main main";
+  grid-template-columns: min-content auto;
+`;
+
+const NavBarGridWrapper = styled.div`
+  grid-area: header;
+`;
+const MainPageGridWrapper = styled.div`
+  grid-area: main;
+`;
 
 export default App;
